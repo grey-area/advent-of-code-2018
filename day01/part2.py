2867932246be0b344a71222c498ab10e784fdfbc
Andrew M. Webb <andrew@awebb.info>
2018-12-02 23:02:51 +0000

    Modified to only store frequencies seen during the first cycle, since the first repeat must be a repeat of a frequency seen during that cycle. Doesn't handle the edge case that the repeat happens during the first cycle

--- day01/part2.py
+++ day01/part2.py
@@ -2,17 +2,19 @@ import numpy as np
 from itertools import cycle
 
 # Print the first frequency offset that occurs twice
-# Done here by cycling round the array and storing seen values in a set
+# Done by storing the frequencies seen during the first cycle,
+# then cycling round the array looking for a reoccurance.
+# Any repeat must be a repeat of a frequency seen during the first cycle.
 # Requires looping through the array 135 times
 
 freq_changes = np.loadtxt('input', dtype=np.int32)
 
-freq_seen = {0}
-freq = 0
+freq_seen = np.cumsum(freq_changes)
+freq = freq_seen[-1]
+freq_seen = frozenset(freq_seen)
+    
 for change in cycle(freq_changes):
     freq += change
     if freq in freq_seen:
+        print(freq)
         break
-    freq_seen.add(freq)
-
-print(freq)
