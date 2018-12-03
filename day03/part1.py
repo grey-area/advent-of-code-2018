04e3ae1b6ac21c5b78aa31cf9450b47a0e8666f5
Andrew Webb <andrew@awebb.info>
2018-12-03 09:54:22 +0000

    Replaced list comprehension with map

--- day03/part1.py
+++ day03/part1.py
@@ -9,13 +9,12 @@ import re
 with open('input') as f:
     data = f.read().splitlines()
 re_str = '^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$'
-    
+
 fabric = np.zeros((1000, 1000), dtype=np.int32)
 
 for line in data:
-    id_, x, y, w, h = [int(s) for s in re.search(re_str, line).groups()]
-    
+    id_, x, y, w, h = map(int, re.search(re_str, line).groups())
+
     fabric[y:y+h, x:x+w] += 1
 
 print(np.sum(fabric > 1))
-    
--- day03/part2.py
+++ day03/part2.py
@@ -17,13 +17,13 @@ re_str = '^#(\d+) @ (\d+),(\d+): (\d+)x(\d+)$'
 Claim = namedtuple('Claim', ['x', 'y', 'w', 'h'])
 claims = {}
 
-    
+
 fabric = np.zeros((1000, 1000), dtype=np.int32)
 
 for line in data:
-    id_, x, y, w, h = [int(s) for s in re.search(re_str, line).groups()]
+    id_, x, y, w, h = map(int, re.search(re_str, line).groups())
     claims[id_] = Claim(x, y, w, h)
-    
+
     fabric[y:y+h, x:x+w] += 1
 
 
@@ -31,5 +31,3 @@ for id_, c in claims.items():
     if np.sum(fabric[c.y:c.y+c.h, c.x:c.x+c.w]) == c.w * c.h:
         print(id_)
         break
-    
-    
