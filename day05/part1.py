1de0c8a13ba19fa8f2ebf0e60319bcfbbe4bdc74
Andrew Webb <andrew@awebb.info>
2018-12-05 12:35:11 +0000

    For part 2 we can reduce the string before trialing removing each letter, reducing computation by a factor of ~5

--- day05/part1.py
+++ day05/part1.py
@@ -2,15 +2,19 @@
 # different capitalization.
 # Report the length of the resulting string.
 
-with open('input') as f:
-    data = f.read().strip('\n')
+def reduce(start_str):
+    ans_str = []
 
-ans_str = []
+    for c in start_str:
+        if len(ans_str) > 0 and c != ans_str[-1] and c.lower() == ans_str[-1].lower():
+            ans_str.pop()
+        else:
+            ans_str.append(c)
 
-for c in data:
-    if len(ans_str) > 0 and c != ans_str[-1] and c.lower() == ans_str[-1].lower():
-        ans_str.pop()
-    else:
-        ans_str.append(c)
+    return ''.join(ans_str)
 
-print(len(ans_str))
+if __name__=='__main__':
+    with open('input') as f:
+        data = f.read().strip('\n')
+
+    print(len(reduce(data)))
--- day05/part2.py
+++ day05/part2.py
@@ -1,5 +1,6 @@
 from string import ascii_lowercase
 import re
+import part1
 
 # For each letter, remove all instances of that letter in each case.
 # Then reduce the string by removing pairs of adjacent letters that are the same but have
@@ -9,19 +10,14 @@ import re
 with open('input') as f:
     data = f.read().strip('\n')
 
+# Note, we can reduce the string *before* trialing removing each letter and further reducing
+data = part1.reduce(data)
+
 resulting_lengths = {}
 
 for l in ascii_lowercase:
     data_rem_l = re.sub(f'{l}|{l.upper()}', '', data)
 
-    ans_str = []
-
-    for c in data_rem_l:
-        if len(ans_str) > 0 and c != ans_str[-1] and c.lower() == ans_str[-1].lower():
-            ans_str.pop()
-        else:
-            ans_str.append(c)
-
-    resulting_lengths[l] = len(ans_str)
+    resulting_lengths[l] = len(part1.reduce(data_rem_l))
 
 print(min(resulting_lengths.values()))
