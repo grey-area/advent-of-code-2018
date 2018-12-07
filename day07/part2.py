d9d8014802029cf3078e0463af937986d63dfd9f
Andrew Webb <andrew@awebb.info>
2018-12-07 11:06:29 +0000

    Added variables for num workers and base time

--- day07/part2.py
+++ day07/part2.py
@@ -2,23 +2,27 @@ import utils
 import bisect
 from collections import namedtuple, deque
 
-graph = utils.load_data()
 
 class Job():
     def __init__(self, event, t):
         self.event = event
         self.t = t
 
+
+graph = utils.load_data()
 jobs = []
+workers = 5
+base_t = 60
 
 ans_str = ''
 total_time = 0
 
+
 while graph.num_free_events() + len(jobs) > 0:
 
-    if len(jobs) < 5 and graph.num_free_events() > 0:
+    if len(jobs) < workers and graph.num_free_events() > 0:
         event = graph.pop_free_event()
-        t = ord(event) - ord('A') + 61
+        t = ord(event) - ord('A') + base_t + 1
         jobs.append(Job(event, t))
 
     else:
@@ -32,6 +36,7 @@ while graph.num_free_events() + len(jobs) > 0:
                 ans_str += job.event
 
         jobs = [job for job in jobs if job.t > 0]
+
 
 print(ans_str)
 print(total_time)
