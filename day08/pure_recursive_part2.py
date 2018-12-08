7043d114c568ba42d70604ff9fce85022e4068da
Andrew Webb <andrew@awebb.info>
2018-12-08 18:28:17 +0000

    A (less readable) solution to part 2. Recursion is used while reading the input data, and we compute the result directly rather than building a tree data structure

---
+++ day08/pure_recursive_part2.py
@@ -0,0 +1,15 @@
+def process_node(data):
+    num_children, num_metadata = data.pop(), data.pop()
+
+    values = [process_node(data) for i in range(num_children)]
+    metadata = [data.pop() for i in range(num_metadata)]
+
+    if num_children > 0:
+        return sum([values[m - 1] for m in metadata if m >= 1 and m <= len(values)])
+    else:
+        return sum(metadata)
+
+with open('input') as f:
+    data = list(map(int, f.read().split()[::-1]))
+
+print(process_node(data))
