aba746ad9f4f48af08e2113c9a8e2aeef9e15ccd
Andrew Webb <andrew@awebb.info>
2018-12-11 07:32:53 +0000

    More readable power array construction

--- day11/part1.py
+++ day11/part1.py
@@ -3,10 +3,13 @@ from scipy.signal import convolve2d
 
 INPUT = 7689
 
-rack = np.expand_dims(np.arange(300) + 10, axis=1)
-ys = np.expand_dims(np.arange(300), axis=0)
-power = (np.mod((rack * ys + INPUT) * rack, 1000) / 100).astype(np.int32) - 5
+def cell_power(x, y):
+    rack = x + 10
+    power = (rack * y + INPUT) * rack
+    power = (power % 1000) / 100 # hundreds digit
+    return (power - 5).astype(np.int32)
 
+power = np.fromfunction(cell_power, shape=(300, 300))
 three_by_three = convolve2d(power, np.ones((3, 3)), mode='valid')
 
 print(np.unravel_index(np.argmax(three_by_three), three_by_three.shape))
