e470c44f27eea28bfd3099dacd980db363143ad5
Andrew Webb <andrew@awebb.info>
2018-12-12 23:00:50 +0000

    Refactored

--- day12/part2.py
+++ day12/part2.py
@@ -2,63 +2,81 @@ import re
 import numpy as np
 from scipy.signal import convolve, fftconvolve
 
-def transform(state, results, kernel):
+
+def transform(state, kernel, rules):
     index_offset = 0
 
+    # Ensure that the state has leading and trailing empty pots
     if np.sum(state[:2]) > 0:
         state = np.concatenate((np.zeros(2, dtype=np.bool), state))
         index_offset -= 2
     if np.sum(state[-2:]) > 0:
         state = np.concatenate((state, np.zeros(2, dtype=np.bool)))
 
-    indices = convolve(state, kernel, mode='same')
-    state = results[indices]
+    # Iterate
+    state = rules[convolve(state, kernel, mode='same')]
 
+    # Strip off leading empty pots
     first_non_zero = np.argmax(state)
     if first_non_zero > 2:
         state = state[first_non_zero-2:]
         index_offset += first_non_zero - 2
 
+    # We should really strip off trailing empty pots as well
+    # but for my input we didn't have to
+
     return state, index_offset
 
-with open('input') as f:
-    data = f.read().splitlines()
 
-state_str = re.search(': (.+)$', data[0]).groups()[0]
-state = np.array([1 if x=='#' else 0 for x in state_str])
+def load_data():
+    with open('input') as f:
+        data = f.read().splitlines()
 
-results = np.zeros(32, dtype=np.bool)
-for line in data[2:]:
-    pattern, result = re.search('^(.+) => (.)$', line).groups()
-    pattern_index = sum([2**(4-x_i) if x=='#' else 0 for x_i, x in enumerate(pattern)])
-    results[pattern_index] = int(result=='#')
-kernel = np.array([2**x_i for x_i in range(5)], dtype=np.int8)
+    state_str = re.search(': (.+)$', data[0]).groups()[0]
+    state = np.array([1 if x=='#' else 0 for x in state_str])
 
-seen_states = [state.copy()]
-seen_offsets = [0]
+    rules = np.zeros(32, dtype=np.bool)
+    for line in data[2:]:
+        pattern, result = re.search('^(.+) => (.)$', line).groups()
+        pattern_index = sum([2**(4-x_i) if x=='#' else 0 for x_i, x in enumerate(pattern)])
+        rules[pattern_index] = int(result=='#')
 
-first_index = 0
-steps = 50000000000
-terminate = False
-for i in range(1, steps, 1):
-    state, index_offset = transform(state, results, kernel)
-    first_index += index_offset
+    return state, rules
 
-    for state_i, (seen_state, seen_offset) in enumerate(zip(seen_states, seen_offsets)):
-        if state.size == seen_state.size and np.all(state == seen_state):
-            terminate = True
-            print(first_index, seen_offset, i, state_i)
 
-    if terminate:
-        break
+def main():
+    state, rules = load_data()
+    kernel = np.array([2**x_i for x_i in range(5)], dtype=np.int8)
 
-    seen_states.append(state)
-    seen_offsets.append(first_index)
+    # Record states that we've seen before, and the index of the
+    seen_states = [state.copy()]
+    seen_index_offsets = [0]
 
-iteration_diff = i - state_i
-index_diff = first_index - seen_offset
+    index_offset = 0
+    steps = 50000000000
+    terminate = False
+    for i in range(1, steps, 1):
+        state, index_offset_change = transform(state, kernel, rules)
+        index_offset += index_offset_change
 
-first_index += ((steps - i) // iteration_diff) * index_diff
+        for state_i, (seen_state, seen_offset) in enumerate(zip(seen_states, seen_index_offsets)):
+            if state.size == seen_state.size and np.all(state == seen_state):
+                terminate = True
 
-indices = np.arange(first_index, first_index + state.size, 1)
-print(np.sum(indices * state))
+        if terminate:
+            break
+
+        seen_states.append(state)
+        seen_index_offsets.append(index_offset)
+
+    iteration_diff = i - state_i
+    index_diff = index_offset - seen_offset
+
+    index_offset += ((steps - i) // iteration_diff) * index_diff
+
+    indices = np.arange(index_offset, index_offset + state.size, 1)
+    print(np.sum(indices * state))
+
+
+if __name__ == '__main__':
+    main()
