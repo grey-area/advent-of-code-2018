103774e315c4cf81c9e4187085e92b9b8ba869aa
Andrew Webb <andrew@awebb.info>
2018-12-23 11:51:43 +0000

    Small changes to erosion grid computation. Values were set to -1 then immediately set to their proper values

--- day22/utils.py
+++ day22/utils.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 def get_type_grid(depth, target):
-    erosion_grid = -1 * np.ones((target[0] + 100, target[1] + 100), dtype=np.int64)
+    W, H = target[0] + 100, target[1] + 100
+    erosion_grid = np.zeros((W, H), dtype=np.int64)
     erosion_grid[0, 0] = depth % 20183
-    erosion_grid[:, 0] = np.fromfunction(lambda x: (16807 * x + depth) % 20183, (erosion_grid.shape[0], ))
-    erosion_grid[0, :] = np.fromfunction(lambda y: (48271 * y + depth) % 20183, (erosion_grid.shape[1], ))
+    erosion_grid[:, 0] = (np.arange(W) * 16807 + depth) % 20183
+    erosion_grid[0, :] = (np.arange(H) * 48271 + depth) % 20183
 
-    for i in range(1, erosion_grid.shape[0]):
-        for j in range(1, erosion_grid.shape[1]):
+    for i in range(1, W):
+        for j in range(1, H):
             erosion_grid[i, j] = (erosion_grid[i-1, j] * erosion_grid[i, j-1] + depth) % 20183
 
     erosion_grid[target] = erosion_grid[0, 0]
