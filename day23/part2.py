696d84f791c4c4fe51c8ac370a90012b82fe67ee
Andrew Webb <andrew@awebb.info>
2018-12-23 08:21:14 +0000

    Still dumb annealing approach

--- day23/part2.py
+++ day23/part2.py
@@ -19,8 +19,9 @@ for line_i, line in enumerate(data):
     points[line_i, :] = point
     radii[line_i] = r
 
-m1 = np.min(mins)
-m2 = np.max(maxes)
+min_val = np.min(mins)
+max_val = np.max(maxes)
+
 
 def compute_counts(points, points2, radii):
     dists = np.sum(np.abs(np.expand_dims(points, 0) - np.expand_dims(points2, 1)), axis=2)
@@ -28,19 +29,18 @@ def compute_counts(points, points2, radii):
 
     return counts
 
-
-best_ever = np.mean(points, axis=0).astype(np.int64)
-best_ever_count = 0
+best_point = np.mean(points, axis=0).astype(np.int64)
+best_count = 0
 
 t = 0
-ran = (m2 - m1) // 2
+mutation_rate = (max_val - min_val) // 2
 
-mutation = np.random.randint(-ran, ran+1, size=(50000, 3))
-points2 = np.expand_dims(best_ever, 0) + mutation
+mutation = np.random.randint(-mutation_rate, mutation_rate+1, size=(100000, 3))
+points2 = np.expand_dims(best_point, 0) + mutation
 
-for i in range(200000):
+while True:
     if i % 500 == 0:
-        print(i, best_ever_count, ran, ','.join([str(i) for i in best_ever]))
+        print(i, best_count, mutation_rate, ','.join([str(i) for i in best_point]))
     counts = compute_counts(points, points2, radii)
 
     highest_count = np.max(counts)
@@ -50,23 +50,24 @@ for i in range(200000):
     mans = np.sum(np.abs(matching_points))
     best_i = count_indices[np.argmin(mans)]
 
-    #best_i = np.argmax(counts)
-
     p = points2[best_i, :]
     c = counts[best_i]
-    if c > best_ever_count or c == best_ever_count and np.sum(np.abs(p)) < np.sum(np.abs(best_ever)):
-        best_ever_count = c
-        best_ever = np.copy(p)
+    if c > best_count or c == best_count and np.sum(np.abs(p)) < np.sum(np.abs(best_point)):
+        best_count = c
+        best_point = np.copy(p)
         t = 0
     else:
         t += 1
 
-    if t > 50:
-        ran //= 2
+    if t > 20000 and mutation_rate > 100:
+        mutation_rate //= 2
         t = 0
 
-    mutation = np.random.randint(-ran, ran+1, size=(50, 3))
-    points2 = np.expand_dims(best_ever, 0) + mutation
+    if t > 100000:
+        break
 
-print(','.join([str(i) for i in best_ever]))
-print(np.sum(np.abs(best_ever)))
+    mutation = np.random.randint(-mutation_rate, mutation_rate+1, size=(100, 3))
+    points2 = np.expand_dims(best_point, 0) + mutation
+
+print(','.join([str(i) for i in best_point]))
+print(np.sum(np.abs(best_point)))
